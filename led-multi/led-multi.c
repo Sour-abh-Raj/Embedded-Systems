c10acdb41d1a2f05248556d87baef5d1e34150c6
sour-abh-raj <btech60064.21@bitmesra.ac.in>
2024-03-06 16:29:06 +0530

    added seven segment display

--- led-multi/led-multi.c
+++ led-multi/led-multi.c
@@ -1,14 +1,26 @@
-void main() {
-  unsigned char x, y;
-  unsigned int i;
-  p1 = 0x00;
-  while (1) {
-    x = 0x01;
-    for (y=0; y<8; y++) {
-      p1 = x;
-      for (i=0; i<50000; i++) {
-        x = x << 1;
-      }
-    }
-  }
+#include<reg52.h>
+sbit LED1 = P2^0;
+sbit LED2 = P2^1;
+sbit LED3 = P2^2;
+sbit LED4 = P2^3;
+sbit LED5 = P2^4;
+sbit LED6 = P2^5;
+sbit LED7 = P2^6;
+sbit LED8 = P2^7;
+
+void delay(void);
+void main(void){
+   while(1){
+      LED=0;
+      delay();
+	  LED=1;
+	 delay();
+	 }
+}
+void delay(void){
+int i;
+for(i=0;i<20000;i++)
+	{
+		
+	}
 }
